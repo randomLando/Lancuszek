5176658d3b5d9d4317cd32ee1bcbb9f7b1f83920
randomLando <mikolaj.mejer@outlook.com>
2022-10-07 15:18:25 +0200

    Add project files.

---
+++ Łańcuszek/Łańcuszek.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int tel, lasttel, dat, a, b, c, d, wynik;
+    cin >> tel >> dat;
+    lasttel = tel % 10;
+    a = lasttel * 2;
+    b = a + 5;
+    c = b * 50;
+    d = c + 1764;
+    wynik = d - dat;
+    cout << wynik;
+}
